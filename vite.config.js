89caa88b8301eec41694b9521c115531c9010ef9
Hana <babanyhana12@gmail.com>
2025-01-27 23:41:01 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default {
   build: {
     rollupOptions: {
-      external: ['react-router-dom']
+      external: ['react-router-dom'],
+      external: ['react-icons/fa']
     }
   }
 }
